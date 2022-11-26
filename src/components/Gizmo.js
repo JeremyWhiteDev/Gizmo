33bb8e0d5e9285452f991742f7bf330e4bfb6852
Jeremy White <jcwhite414@gmail.com>
2022-11-26 10:19:13 -0600

    added temp nav bar

--- src/components/Gizmo.js
+++ src/components/Gizmo.js
@@ -3,13 +3,23 @@ import { Authorized } from "./views/Authorized";
 import { ApplicationViews } from "./views/ApplicationViews";
 import { Login } from "./auth/Login";
 import { Register } from "./auth/Register";
+import { NavBar } from "./NavBar";
 
 export const Gizmo = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route exact path="/" element={<h1>Home Page</h1>} />
+      <Route
+        exact
+        path="/"
+        element={
+          <>
+            <NavBar />
+            <h1>Home Page</h1>
+          </>
+        }
+      />
       <Route exact path="/gizmos" element={<h1>Gizmo Page</h1>} />
 
       <Route
