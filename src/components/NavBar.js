8dc252ef68fc68a88f7d638b96d8d8ea034798c6
Jeremy White <jcwhite414@gmail.com>
2022-11-27 10:38:18 -0600

    refactored nav to use navlinks

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import { Navigate, NavLink, useNavigate } from "react-router-dom";
 
 export const NavBar = () => {
   const [drawerOpen, setDrawerOpen] = useState();
+  const navigate = useNavigate();
 
   const checkAuth = () => {
     if (localStorage.getItem("capstone_user")) return true;
@@ -28,6 +30,7 @@ export const NavBar = () => {
                 <button
                   type="button"
                   className="bg-white rounded-lg hover:bg-amber-700 mr-3"
+                  onClick={() => navigate("/login")}
                 >
                   <div className="text-transparent bg-clip-text focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-md px-5 py-2.5 text-center  md:mr-0 bg-gradient-to-r bg-white from-purple-800 via-yellow-600 to-pink-600">
                     Log In/Sign Up
@@ -70,47 +73,87 @@ export const NavBar = () => {
             <div className="flex flex-col justify-between p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-purple-700 md:dark:bg-purple-800 dark:border-gray-700 h-5/6 box-border">
               <ul className="flex flex-col md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-purple-700 md:dark:bg-purple-800 dark:border-gray-700 h-5/6 box-border ">
                 <li>
-                  <a
-                    href="/"
-                    className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-amber-400 md:p-0 dark:text-white"
-                    aria-current="page"
+                  <NavLink
+                    to="/"
+                    onClick={(click) => {
+                      //   click.preventDefault();
+                      setDrawerOpen(false);
+                    }}
+                    className={({ isActive }) =>
+                      isActive
+                        ? "block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-amber-400 md:p-0 dark:text-white"
+                        : "block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-200 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    }
                   >
                     Home
-                  </a>
+                  </NavLink>
                 </li>
                 <li>
-                  <a
-                    href="#"
-                    className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-200 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  <NavLink
+                    to="gizmos"
+                    onClick={(click) => {
+                      //   click.preventDefault();
+                      setDrawerOpen(false);
+                    }}
+                    className={({ isActive }) =>
+                      isActive
+                        ? "block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-amber-400 md:p-0 dark:text-white"
+                        : "block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-200 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    }
                   >
-                    Browse Gizmos
-                  </a>
+                    Gizmos
+                  </NavLink>
                 </li>
+
                 {checkAuth() ? (
                   <>
                     <li>
-                      <a
-                        href="#"
-                        className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-200 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                      <NavLink
+                        to="feed"
+                        onClick={(click) => {
+                          //   click.preventDefault();
+                          setDrawerOpen(false);
+                        }}
+                        className={({ isActive }) =>
+                          isActive
+                            ? "block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-amber-400 md:p-0 dark:text-white"
+                            : "block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-200 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                        }
                       >
                         Activity Feed
-                      </a>
+                      </NavLink>
                     </li>
                     <li>
-                      <a
-                        href="#"
-                        className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-200 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                      <NavLink
+                        to="garage"
+                        onClick={(click) => {
+                          //   click.preventDefault();
+                          setDrawerOpen(false);
+                        }}
+                        className={({ isActive }) =>
+                          isActive
+                            ? "block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-amber-400 md:p-0 dark:text-white"
+                            : "block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-200 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                        }
                       >
                         Garage
-                      </a>
+                      </NavLink>
                     </li>
                     <li>
-                      <a
-                        href="#"
-                        className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-200 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                      <NavLink
+                        to="requests"
+                        onClick={(click) => {
+                          //   click.preventDefault();
+                          setDrawerOpen(false);
+                        }}
+                        className={({ isActive }) =>
+                          isActive
+                            ? "block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-amber-400 md:p-0 dark:text-white"
+                            : "block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-200 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                        }
                       >
                         Requests
-                      </a>
+                      </NavLink>
                     </li>
                   </>
                 ) : (
