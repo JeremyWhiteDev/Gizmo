cbfe1f95e5a96b8741a8b9bbbe3c7831e29bc16c
Jeremy White <jcwhite414@gmail.com>
2022-12-06 21:32:17 -0600

    duplicate ids in register form

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -42,7 +42,7 @@ export const Register = () => {
               Full Name
             </label>
             <input
-              id="email"
+              id="fullName"
               onChange={(e) => updateUser(e)}
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Your Full Name"
