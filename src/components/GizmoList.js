8ef95d634e05b2302f537973a701781ddd8fb5a2
Jeremy White <jcwhite414@gmail.com>
2022-12-13 17:41:13 -0600

    fized htmlFor

--- src/components/GizmoList.js
+++ src/components/GizmoList.js
@@ -183,7 +183,7 @@ export const GizmoList = () => {
         return (
           <>
             <input
-              id="isPublic"
+              id={`category--${category.id}`}
               type="checkbox"
               value={category.id}
               onChange={(e) => {
@@ -200,7 +200,7 @@ export const GizmoList = () => {
               className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
             />
             <label
-              htmlFor="isPublic"
+              htmlFor={`category--${category.id}`}
               className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
             >
               {category.name}
