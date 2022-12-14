e073be1ac4177cb9fa4ac3c98ecb3200c34d765a
Jeremy White <jcwhite414@gmail.com>
2022-12-13 20:11:06 -0600

    acceptable styling of search term input and category checkboxes

--- src/components/GizmoList.js
+++ src/components/GizmoList.js
@@ -186,7 +186,13 @@ export const GizmoList = () => {
       <h1 className="pl-4 dark:text-white mx-auto max-w-xl md:max-w-screen-xl mb-6">
         Browse Public Gizmos
       </h1>
-      <div>
+      <div className="mx-auto max-w-xl md: md:max-w-screen-xl">
+        <label
+          htmlFor={`searchField`}
+          className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+        >
+          Search
+        </label>
         <input
           id={`searchField`}
           type="text"
@@ -194,15 +200,10 @@ export const GizmoList = () => {
           onChange={(e) => {
             setSearchTerm(e.target.value);
           }}
-          className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer  bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Enter Search Terms Here"
         />
-        <label
-          htmlFor={`searchField`}
-          className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-        >
-          Search
-        </label>
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 my-4">
           {categories.map((category, index) => {
             return (
               <div className="">
