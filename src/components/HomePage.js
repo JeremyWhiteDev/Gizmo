d4e0cf6c71786a241f49638a6979390d36502b57
Jeremy White <jcwhite414@gmail.com>
2023-01-06 21:08:16 -0600

    restyled hero section login button for light/dark modes

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -8,7 +8,7 @@ export const HomePage = () => {
   return (
     <section className="">
       <div className="relative bg-gradient-to-br from-indigo-700 via-purple-700 to-pink-700 -mt-3 blur h-96 md:h-screen"></div>
-      <div className="absolute right-0 left-0 top-20 lg:top-48 grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+      <div className="absolute right-0 left-0 top-16 lg:top-48 grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
           <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">
             Share your stuff.
@@ -50,7 +50,9 @@ export const HomePage = () => {
               onClick={() => {
                 navigate("/login");
               }}
-              className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center cursor-pointer text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+              className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center cursor-pointer text-white border border-gray-300 rounded-lg focus:ring-4 focus:ring-gray-100  
+            
+              dark:focus:ring-gray-800"
             >
               Log In/Sign Up
             </a>
